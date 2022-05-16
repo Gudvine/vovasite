9539956e04d0e5f39a5091167b4c53be0b1470e4
Gudvine <karapuz719@gmail.com>
2022-05-16 23:18:58 +0300

    db_onetoone field version of work_tag

--- newApp/models.py
+++ newApp/models.py
@@ -5,19 +5,20 @@ class Score(models.Model):
 	score_value = models.FloatField()
 
 
-class Videomaker(models.Model):
-	first_name = models.CharField(max_length=40)
-	last_name = models.CharField(max_length=40)
-	free_status = models.BooleanField(default=True)
-	work_tag = models.ManyToManyField("Worktag", blank=True)
-	score = models.OneToOneField(Score, on_delete=models.CASCADE, primary_key=True)
-
-	class Meta:
-		verbose_name_plural = "Videomakers"
-
-
 class Worktag(models.Model):
 	tag_name = models.CharField(max_length=50)
 
 	class Meta:
 		unique_together = ("tag_name", )
+
+
+class Videomaker(models.Model):
+	first_name = models.CharField(max_length=40)
+	last_name = models.CharField(max_length=40)
+	free_status = models.BooleanField(default=True)
+	# work_tag = models.ManyToManyField("Worktag", blank=True)
+	work_tag = models.OneToOneField(Worktag, on_delete=models.CASCADE)
+	score = models.OneToOneField(Score, on_delete=models.CASCADE, primary_key=True)
+
+	class Meta:
+		verbose_name_plural = "Videomakers"
