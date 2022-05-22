e65864090a6e625a865ae9351d4ad8ee089042ee
Gudvine <karapuz719@gmail.com>
2022-05-22 17:27:47 +0300

    added popup menu and cabinet

--- newApp/forms.py
+++ newApp/forms.py
@@ -2,3 +2,6 @@ from django import forms
 
 from .models import Videomaker
 
+
+class SearchForm(forms.Form):
+	query = forms.ChoiceField()
--- newApp/urls.py
+++ newApp/urls.py
@@ -1,9 +1,8 @@
-from unicodedata import name
 from django.urls import path
 
 from newApp import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('search_results/', views.SearchResultsView.as_view(), name="search_results")  # подумать
+	path('search_results/', views.SearchResultsView.as_view(), name="search_results")
 ]
