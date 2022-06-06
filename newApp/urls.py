818008493c6c2812c2174f667e36cd6650914a36
Gudvine <karapuz719@gmail.com>
2022-06-06 08:22:12 +0300

    added some new views

--- newApp/urls.py
+++ newApp/urls.py
@@ -5,5 +5,6 @@ from newApp import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('search_results/', views.SearchResultsView.as_view(), name="search_results"),
+	path('filter/', views.FilterVideomakerView.as_view(), name="filter"),
 	path('cabinet/', views.cabinet, name="cabinet"),
 ]
